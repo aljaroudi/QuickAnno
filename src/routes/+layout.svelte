7c48a82a903e039c37757a3b3d9dd4076dbe47d0
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2023-08-31 10:37:46 -0700

    add <main> styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.css'
 </script>
 
-<slot />
+<main
+	class="flex flex-col items-center w-full h-screen gap-2 p-2 font-mono text-xs text-teal-600 bg-teal-50 dark:bg-teal-950"
+>
+	<slot />
+</main>
