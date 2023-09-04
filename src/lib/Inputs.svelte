f390b30838e642891eeeadb6f3de9ae39b3d229f
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2023-09-04 15:41:38 -0700

    "none" option, Array → Set

--- src/lib/Inputs.svelte
+++ src/lib/Inputs.svelte
@@ -2,7 +2,7 @@
 	import { downloadAnnotations } from '$lib'
 
 	export let labels: Set<string>
-	export let currentLabels: string[]
+	export let currentLabels: Set<string>
 	export let onClick: (label: string) => void
 	export let totalImages: number
 	export let currentCount: number
@@ -11,9 +11,9 @@
 <div class="flex flex-col gap-2 px-2 w-52">
 	{#each labels as label, i}
 		<button
-			class="sticky bottom-0 flex items-center w-full h-8 gap-2 px-2 py-1 text-white transition bg-teal-600 rounded-lg aria-pressed:bg-teal-300 aria-pressed:text-black hover:bg-teal-500"
+			class="flex items-center w-full h-8 gap-2 px-2 py-1 text-white transition bg-teal-600 rounded-lg aria-pressed:bg-teal-300 aria-pressed:text-black hover:bg-teal-500"
 			on:click={() => onClick(label)}
-			aria-pressed={currentLabels.includes(label)}
+			aria-pressed={currentLabels.has(label)}
 		>
 			{#if i < 9}
 				<span
@@ -25,6 +25,18 @@
 			{label}
 		</button>
 	{/each}
+	<button
+		class="flex items-center w-full h-8 gap-2 px-2 py-1 text-white transition bg-teal-600 rounded-lg shadow aria-pressed:bg-rose-300 aria-pressed:text-black hover:bg-teal-500"
+		on:click={() => onClick('none')}
+		aria-pressed={currentLabels.has('none')}
+	>
+		<span
+			class="w-5 h-5 text-black bg-gray-200 border border-b-2 border-r-2 border-gray-400 rounded-md"
+		>
+			0
+		</span>
+		none
+	</button>
 
 	<div class="flex flex-col gap-2 mt-auto">
 		<button
